3056b667731fa8f6416799087fa1ce8bbc8f9634
TGBenW <dukariga@gmail.com>
2021-06-05 02:02:01 +0300

    create: newsletter section

--- js/main.js
+++ js/main.js
@@ -58,12 +58,10 @@ $(document).ready(function () {
   const swiper = new Swiper(".swiper-container", {
     direction: "horizontal",
     loop: true,
-
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
-
     autoplay: {
       delay: 2000,
     },
